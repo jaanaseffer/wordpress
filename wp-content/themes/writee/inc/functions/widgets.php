1c357b1dcc5d77f033c4dbea14448280a77863ca
Gitium <gitium@presslabs.com>
2019-12-06 11:35:33 +0200

    deleted theme `Writee` version 2.0.3 ion 2.2

--- wp-content/themes/writee/inc/functions/widgets.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/*******************************************/
-## Custom widget list
-/*******************************************/
-
-require('widgets/recent-posts.php');
-require('widgets/social-icons.php');
-require('widgets/about-me.php');
-?>
